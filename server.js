a6d9467a4b2baf68b788d2d88ab15ba9d023d853
Mazidulla <mazidula.mk@gmail.com>
2023-10-28 22:31:50 +0530

    changes

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.post("/",encoder, function(req,res){
     var password = req.body.password;
 
     connection.query("select * from loginuser where user_name = ? and user_pass = ?",[username,password],function(error,results,fields){
-        if (results.length > 0) {
+        if (results.length) {
             res.redirect(`/welcome?username=${username}`);
         } else {
             res.redirect("/");
