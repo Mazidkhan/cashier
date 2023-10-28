99a284763cf994831218cc22298f133414fa5595
Mazidulla <mazidula.mk@gmail.com>
2023-10-28 21:33:49 +0530

    port

--- server.js
+++ server.js
@@ -150,4 +150,7 @@ app.get("/sample",function(req,res){
 })
 
 // set app port 
-app.listen(4000);
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
